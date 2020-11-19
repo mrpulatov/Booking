cb0527e84ee03012f826b59a63d3730b33d50f26
pulatov03 <pulatovubaydulla@gmail.com>
2020-11-19 11:06:02 +0500

    Adding comment 2

--- main_menu/adding_new_booking.h
+++ main_menu/adding_new_booking.h
@@ -5,6 +5,7 @@ using namespace std;
 
 
 void ask_date_details(int *day, int *month, int *year){
+	//function of asking date and checking is it correct
 	re_day:
 	cout << "Day: ";
 	*day = 0;
@@ -37,6 +38,7 @@ void ask_date_details(int *day, int *month, int *year){
 }
 
 int calculate_day(int y, int m, int d){
+	//function wich calculates daysfrom start of year till the date which he take
 	m = (m + 9) % 12;
 	y = y - m/10;
 	return 365*y + y/4 - y/100 + y/400 + (m*306 + 5)/10 + ( d - 1 );
@@ -45,18 +47,18 @@ int calculate_day(int y, int m, int d){
 void ask_date(){
 	int day, month, year, days;
 	ofstream myhotel; 
-	myhotel.open("hotel.txt", ios::app); 
+	myhotel.open("hotel.txt", ios::app); // opening file where we will save all our bookings
 	system("CLS");
 	cout << "Adding new booking window" << endl;
 	cout << "Please enter the date of ariving: " << endl;
-	ask_date_details(&day, &month, &year);
-	myhotel << "{" << day << "," << month << "," << year << ",";
-	days = calculate_day(year, month, day);
+	ask_date_details(&day, &month, &year); // function which takes data from client and checking it  1<=day<=31, 1<=month<=12, year => 2020
+	myhotel << "\n {" << day << "," << month << "," << year << ","; // saving data to file
+	days = calculate_day(year, month, day); // calculating days from start of year
 	cout << "Please enter the date of lieving:" << endl;
-	ask_date_details(&day, &month, &year);
-	days = calculate_day(year, month, day) - days;
-	myhotel << day << "," << month << "," << year << "," << days << "} \n";
-	myhotel.close();
+	ask_date_details(&day, &month, &year); // function which takes data from client and checking it  1<=day<=31, 1<=month<=12, year => 2020
+	days = calculate_day(year, month, day) - days; // days untill days for finishing date - day untill start will give amount of days for living
+	myhotel << day << "," << month << "," << year << "," << days << "} \n"; // saving data to file
+	myhotel.close(); //closing document
 }
 
 
